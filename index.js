2a0ed4d7935130ec0d0fcd8908ba0a9cc3fd9fbc
han <caokyhan@gmail.com>
2016-11-17 17:09:14 +0700

    update topic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Consumer = kafka.Consumer,
 // the zookeeper port 2181
     client = new kafka.Client('localhost:2181'),
 // The consumer object specifies the client and topic(s) it subscribes to
-    consumer = new Consumer(client, [ { topic: 'GrokkingLog', partition: 0 } ], { groupId: 'group0', autoCommit: false });
+    consumer = new Consumer(client, [ { topic: '/GrokkingLog', partition: 0 } ], { groupId: 'group0', autoCommit: false });
 
 consumer.on('message', function (message) {
     // grab the main content from the Kafka message
