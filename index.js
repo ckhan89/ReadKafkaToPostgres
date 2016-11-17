3e11e30c166c1b80cd28ecec74220bbae8dd82ca
han <caokyhan@gmail.com>
2016-11-17 17:16:34 +0700

    update code

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var kafka = require('kafka-node');
 var Consumer = kafka.Consumer,
 // The client specifies the ip of the Kafka producer and uses
 // the zookeeper port 2181
-    client = new kafka.Client('localhost:2181'),
+    client = new kafka.Client("localhost:2181"),
 // The consumer object specifies the client and topic(s) it subscribes to
-    consumer = new Consumer(client, [ { topic: 'GrokkingLog', partition: 0 } ], { groupId: 'group0', autoCommit: true });
+    consumer = new Consumer(client, [ { topic: "GrokkingLog", partition: 0 } ], { groupId: "group0", autoCommit: true });
 
 consumer.on('message', function (message) {
     // grab the main content from the Kafka message
