e31e1e698deba755054c29c661430007deb90e7c
han <caokyhan@gmail.com>
2016-11-18 14:37:26 +0700

    debug code

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ var Consumer = kafka.Consumer,
 
 consumer.on('message', function (message) {
     // grab the main content from the Kafka message
-    // var params = queryString.parse(message.value)
-    console.log(message)
-    // logList.push({'uuid':params.uuid, 'location': params.location, 'referrer': params.referrer, 'url': params.url,
-    //                 'product': params.product, 'video': params.video, 'viewer': params.viewer})
-    // if (logList.length > 10000) {
-    //     writeLog.writeData(logList,function (error) {
-    //         if(error) console.log(error)
-    //         else {
-    //             console.log('save log success')
-    //         }
-    //     })
-    //     logList = new Array()
-    // }
+    var params = queryString.parse(message.value)
+    console.log(params)
+    logList.push({'uuid':params.uuid, 'location': params.location, 'referrer': params.referrer, 'url': params.url,
+                    'product': params.product, 'video': params.video, 'viewer': params.viewer})
+    if (logList.length > 10000) {
+        writeLog.writeData(logList,function (error) {
+            if(error) console.log(error)
+            else {
+                console.log('save log success')
+            }
+        })
+        logList = new Array()
+    }
 });
