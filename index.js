43bd0d0f28b8c9fb33c8712e9611fb6b354761c6
han <caokyhan@gmail.com>
2016-11-19 23:00:07 +0700

    update code

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let kafka = require('kafka-node');
 let writeLog = require('./writeLog')
 let queryString = require('query-string');
 let useragent = require('useragent');
+let CronJob = require('cron').CronJob;
+let Async = require('async')
+
 useragent(true)
 
 var pageviewList = new Array()
@@ -13,7 +16,7 @@ var Consumer = kafka.Consumer,
 // the zookeeper port 2181
     client = new kafka.Client("localhost:2181"),
 // The consumer object specifies the client and topic(s) it subscribes to
-    consumer = new Consumer(client, [ { topic: "GrokkingLog", partition: 0 } ], { groupId: "group0", autoCommit: true });
+    consumer = new Consumer(client, [ { topic: "GrokkingLog", partition: 0 } ], { groupId: "group0", autoCommit: false });
 
 consumer.on('message', function (message) {
     // grab the main content from the Kafka message
@@ -42,35 +45,64 @@ consumer.on('message', function (message) {
             orderList.push(obj)
         }
     }
-    // logList.push({'uuid':params.uuid, 'location': params.location, 'referrer': params.referrer, 'url': params.url,
-    //                 'product': params.product, 'video': params.video, 'viewer': params.viewer})
-    if (pageviewList.length > 10) {
-        writeLog.writeDataPageView(pageviewList,function (error) {
-            if(error) console.log(error)
-            else {
-                console.log('save pageviewList log success')
-            }
-        })
-        pageviewList = new Array()
-    }
 
-    if (clickList.length > 10) {
-        writeLog.writeDataClick(clickList,function (error) {
-            if(error) console.log(error)
-            else {
-                console.log('save clickList log success')
+    new CronJob('0 */5 * * * *', function() {
+        console.log('You will see this message every minute');
+        var taskList = []
+        taskList.push(function (callback) {
+            if (pageviewList.length > 0) {
+                writeLog.writeDataPageView(pageviewList,function (error) {
+                    if(error) callback(error)
+                    else {
+                        console.log('save pageviewList log success')
+                    }
+                })
+                pageviewList = new Array()
+                callback(null)
+            } else {
+                callback(null)
             }
         })
-        clickList = new Array()
-    }
-
-    if (orderList.length > 0) {
-        writeLog.writeDataOrder(orderList,function (error) {
-            if(error) console.log(error)
-            else {
-                console.log('save orderList log success')
+        
+        taskList.push(function (callback) {
+            if (clickList.length > 0) {
+                writeLog.writeDataClick(clickList,function (error) {
+                    if(error) callback(error)
+                    else {
+                        console.log('save clickList log success')
+                    }
+                })
+                clickList = new Array()
+                callback(null)
+            } else {
+                callback(null)
             }
         })
-        orderList = new Array()
-    }
+        taskList.push(function (callback) {
+            if (orderList.length > 0) {
+                writeLog.writeDataOrder(orderList,function (error) {
+                    if(error) callback(error)
+                    else {
+                        console.log('save orderList log success')
+                    }
+                })
+                orderList = new Array()
+                callback(null)
+            } else {
+                callback(null)
+            }
+        })
+        Async.series(taskList,function (error,results) {
+            if (error) console.log(error)
+            else {
+                console.log('save data')
+                // consumer.commit(function (err,data) {
+                //     if (err) console.log(err)
+                //     else {
+                //         console.log('data:',data)
+                //     }
+                // })
+            }
+        })
+    }, null, true, 'America/Los_Angeles');
 });
